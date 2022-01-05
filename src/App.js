8d314d3e19ee49d42e56f23e2cf0bce3e9c8a063
N8TR0N <n8tr0ncrypto@gmail.com>
2022-01-05 18:34:44 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { About, Blog, Features, Footer, Header, Possibility } from './containers';
 import { Brand, CTA, Navbar } from './components';
+import './App.css';
 
 const App = () => {
     return (
