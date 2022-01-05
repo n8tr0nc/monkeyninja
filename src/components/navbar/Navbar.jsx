1d641411f78bc55b2eba1e134d40b6ab84575dc5
N8TR0N <n8tr0ncrypto@gmail.com>
2022-01-05 18:32:50 -0500

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.png'
+import './navbar.css';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(flase);
+
     return (
-        <div>
-            Navbar
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="gpt3__navbar-links_container">
+                    <p><a href="#fivestars">Five Stars</a></p>
+                    <p><a href="#services">Services</a></p>
+                    <p><a href="#clients">Clients</a></p>
+                    <p><a href="#insights">Insights</a></p>
+                    <p><a href="#gmb">Google My Business</a></p>
+                </div>
+            </div>
+            <div className="gpt3__navbar-sign">
+                <p>Sign In</p>
+                <button type="button">Sign Up</button>
+            </div>
+            <div className="gpt3__navbar-menu">
+                {toggleMenu}
+            </div>
         </div>
     )
 }
