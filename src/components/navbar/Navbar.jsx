62f01fca87b5ab40d9c5894d88d1096d6ab8fb63
N8TR0N <n8tr0ncrypto@gmail.com>
2022-01-06 23:15:49 -0500

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,8 +3,17 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.png';
 import './navbar.css';
 
+const Menu = () => (
+    <>
+    <p><a href="#about">About</a></p>
+    <p><a href="#utility">NFT Utility</a></p>
+    <p><a href="#dynasty">Meta Dynasty</a></p>
+    <p><a href="#tokenomics">Tokenomics</a></p>
+    </>
+)
+
 const Navbar = () => {
-    {/*const [toggleMenu, setToggleMenu] = useState(flase);*/}
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     return (
         <div className="navbar">
@@ -13,19 +22,25 @@ const Navbar = () => {
                     <a href="#top"><img src={logo} alt="logo" /></a>
                 </div>
                 <div className="navbar-links_container">
-                    <p><a href="#about">About</a></p>
-                    <p><a href="#utility">NFT Utility</a></p>
-                    <p><a href="#dynasty">Meta Dynasty</a></p>
-                    <p><a href="#tokenomics">Tokenomics</a></p>
-                    {/*<p><a href="#authenticate">Authenticate</a></p>*/}
+                    <Menu />
                 </div>
             </div>
             <div className="navbar-sign">
                 <button type="button">Authenticate</button>
             </div>
-            {/*<div className="navbar-menu">
-                {toggleMenu}
-        </div>*/}
+            <div className="navbar-menu">
+                {toggleMenu
+                    ? <RiCloseLine color="#fff" size={40} onClick={() => setToggleMenu(false)} />
+                    : <RiMenu3Line color="#fff" size={40} onClick={() => setToggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <div className="navbar-menu_container scale-up-center">
+                        <div className="navbar-menu_container-links">
+                            <Menu />
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
